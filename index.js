3fd4ff718e8729c1e8aee5fd104467a62ea23183
Brennan B <bdbasinger777@gmail.com>
2018-06-25 13:18:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // write your code below!
 
 var name = "Joe"
-var height = "74"
+var height = 74
+
+
+// If we had to convert a string to an int
+// console.log(parseInt(height) + 1 )
 
 console.log("Name: " + name)
 console.log(name)
